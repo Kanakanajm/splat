7cd2be75d3779ae7062ac32fb452685d56d80348
jackie <kanakanajm@outlook.com>
2026-04-29 21:02:29 +0200

    Add ambient light

--- shaders/color.fs
+++ shaders/color.fs
@@ -10,5 +10,12 @@ uniform vec3 objectColor;
 uniform vec3 lightColor;
 
 void main() {
-    FragColor = vec4(objectColor*lightColor, 1.0);
+    float ambientStrength = 0.1;
+    // there can also be ambient material and ambient light
+    // here we take the same lightColor as diffuse
+    vec3 ambient = ambientStrength * lightColor;
+
+    vec3 result = ambient * objectColor;
+
+    FragColor = vec4(result, 1.0);
 }
