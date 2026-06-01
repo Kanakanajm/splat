3e6ca935bb0616c8a7d55d65916b50b7f32e7d1c
jackie <kanakanajm@outlook.com>
2026-06-01 16:37:07 +0200

    Use built-in findMSB

--- shaders/medium_debug.fs
+++ shaders/medium_debug.fs
@@ -1,5 +1,5 @@
 #version 330 core
-#extension GL_ARB_gpu_shader5 : require // for findMSB
+#extension GL_ARB_gpu_shader5 : enable
 
 out vec4 FragColor;
 
@@ -7,17 +7,6 @@ in vec2 TexCoord;
 
 uniform usampler2DArray mediumTextures;
 uniform int layer;
-
-int myFindMSB(uint mask) {
-    if (mask == 0u) return -1;
-    int bit = 0;
-    if ((mask & 0xFFFF0000u) != 0u) { bit += 16; mask >>= 16; }
-    if ((mask & 0x0000FF00u) != 0u) { bit +=  8; mask >>=  8; }
-    if ((mask & 0x000000F0u) != 0u) { bit +=  4; mask >>=  4; }
-    if ((mask & 0x0000000Cu) != 0u) { bit +=  2; mask >>=  2; }
-    if ((mask & 0x00000002u) != 0u) { bit +=  1;              }
-    return bit;
-}
 
 vec3 palette(int id)
 {
@@ -32,7 +21,7 @@ vec3 palette(int id)
 void main()
 {
     uint mask = texture(mediumTextures, vec3(TexCoord, layer)).r;
-    int mediumId = myFindMSB(mask);
+    int mediumId = findMSB(mask);
     vec3 color = palette(mediumId);
 
     FragColor = vec4(color, 1.0);
